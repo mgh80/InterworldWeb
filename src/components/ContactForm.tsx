1e7429ed85d8d9cbf0abffb73f3b76822cd670f0
Mauricio García <mauricio.1054@gmail.com>
2025-07-13 21:19:57 -0500

    Add ContactForm

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <div>
+      <h1>Página de contacto</h1>
+      {/* Puedes agregar el componente ContactForm si ya lo tienes */}
+    </div>
+  );
+}
