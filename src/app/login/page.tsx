ac8fda51ae87e16ab3c26d40ca37cd4b3a1b7303
Mauricio García <mauricio.1054@gmail.com>
2025-07-13 21:26:44 -0500

    Add loginPage

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center px-4">
+      <h1 className="text-4xl font-bold text-blue-900">Iniciar sesión</h1>
+    </div>
+  );
+}
