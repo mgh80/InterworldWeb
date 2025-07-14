141789d6143f0139feeebed4ab739728edf4a9ab
Mauricio García <mauricio.1054@gmail.com>
2025-07-13 21:24:13 -0500

    Add ContatcPage

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+// src/app/contact/page.tsx
+"use client";
+
+export default function ContactPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center px-4">
+      <div className="max-w-xl w-full text-center">
+        <h1 className="text-4xl font-bold mb-6 text-blue-900">Contáctanos</h1>
+        <p className="text-lg text-gray-600">
+          Muy pronto podrás comunicarte con nosotros a través de esta sección.
+        </p>
+      </div>
+    </div>
+  );
+}
